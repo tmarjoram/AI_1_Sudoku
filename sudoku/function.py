1e4a9147fb77f5c06e2e7e8fea910b351972db94
Tom Marjoram <tom.marjoram@gmail.com>
2018-08-04 13:42:36 +0100

    Project Files

---
+++ sudoku/function.py
@@ -0,0 +1,60 @@
+from sudoku.utils import *
+
+def grid_values(grid):
+    """Convert grid string into {<box>: <value>} dict with '123456789' value for empties.
+
+    Args:
+        grid: Sudoku grid in string form, 81 characters long
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'
+        - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
+    """
+    return {box:'123456789' if value=='.' else value for box,value in zip(boxes,grid)}
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    for box,value in values.items():
+        if len(value)==1 and value in [str(i) for i in range(1,10)]:
+            for peer in peers[box]:
+                if value in values[peer] and len(values[peer])>1:
+                    values[peer] = values[peer].replace(value,"")
+
+    return values
+
+def eliminate2(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+
+if __name__ == '__main__':
+
+    puz = grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')
+    display(eliminate(puz))
+    print("\n")
+    puz = grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')
+    display(eliminate2(puz))
+
+
